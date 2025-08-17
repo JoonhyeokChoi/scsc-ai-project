52b9924377fba25383785df8c1f1ad0b1fcfc54b
JoonhyeokChoi <64754629+JoonhyeokChoi@users.noreply.github.com>
2025-08-17 15:04:42 +0900

    Changed the GCP project id

--- src/firestore.ts
+++ src/firestore.ts
@@ -2,7 +2,7 @@
 import { Firestore } from "@google-cloud/firestore";
 
 // 환경변수로 명시 (없으면 기본값 사용)
-const projectId = process.env.PROJECT_ID || "scsc-ai";
+const projectId = process.env.PROJECT_ID || "sincere-point-468505-j9";
 const databaseId = process.env.FIRESTORE_DATABASE_ID || "scsc-ai";
 
 // 명시적으로 Firestore 인스턴스 생성
@@ -11,5 +11,5 @@ export const db = new Firestore({
   databaseId,   // ← 만약 비기본 DB를 만들었다면 여기 이름을 넣으세요
 });
 
-// 필요 시: undefined 필드 무시
+// 필요 시: undefined 필드 무시ㄹ
 // db.settings({ ignoreUndefinedProperties: true });
